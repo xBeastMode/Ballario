12f5f6e0a71dfb00d1cbe6e5adc701441684075c
xBeastMode <xBeastMode@users.noreply.github.com>
2016-12-06 22:56:32 -0600

    Updated grid to able to set custom size.

--- src/org/ballario/Map/Grid.java
+++ src/org/ballario/Map/Grid.java
@@ -1,9 +1,15 @@
 package org.ballario.Map;
 import java.awt.*;
 public class Grid{
+	public int width;
+	public int height;
+	public Grid(int width, int height){
+		this.width = width;
+		this.height = height;
+	}
 	public void render(Graphics g){
-		for(int i = 0; i <= 6400; i += 50){
-			for(int j = 0; j <= 3600; j += 50){
+		for(int i = 0; i <= this.width; i += 50){
+			for(int j = 0; j <= this.height; j += 50){
 				g.setColor(Color.LIGHT_GRAY);
 				g.drawLine(0, j, i, j);
 				g.drawLine(i, 0, i, j);
